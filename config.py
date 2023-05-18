52a785a0ad46b69110dc67576fc103fdd04ca465
colakhalil <82612235+colakhalil@users.noreply.github.com>
2023-05-18 14:31:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-# Enter your student ID here
-ID = "28879"
-
 # Path to the .txt file that will have the collected URLs
 LINK_LIST_PATH = "data/link_list.txt"
 
@@ -10,6 +7,3 @@ RAW_HTML_DIR = "data/raw_html/"
 # Path to the JSON file containing the parsed data
 PARSED_HTML_PATH = "data/parsed_data.jsons"
 
-# Secret variable that VRLLab uses for the projecg
-IS_YASSER = False
-IS_ONUR = False
